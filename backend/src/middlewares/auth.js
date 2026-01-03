03592e0c76bba6975413dd2bb332c90471cc079a
Prateek Nigam <prateekn34@gmail.com>
2026-01-03 16:16:59 +0530

    backend: logout route and auth updates

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,25 +1,24 @@
-const User = require("../models/user")
-const jwt = require("jsonwebtoken")
-const authprotec = async (req,res,next)=>{
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-          return res.status(500).send("login first");
-        }
-
-        const decoded = await jwt.verify(token, "hehehe");
-
-        const user = await User.findOne({ _id: decoded.userId });
-        if (!user) {
-          return res.status(500).send("login first");
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        res.send(error.message);
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const authprotec = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send("login first");
     }
 
-}
+    const decoded = await jwt.verify(token, "hehehe");
 
-module.exports = {authprotec};
+    const user = await User.findOne({ _id: decoded.userId });
+    if (!user) {
+      return res.status(500).send("login first");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+module.exports = { authprotec };
